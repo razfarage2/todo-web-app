b0eb583e096d42b3dc377877ac5c72932f4940a2
razfa <razfarage3232@gmail.com>
2024-09-27 00:54:14 +0300

    completed the entire backend added the templates
    next steps: style the page

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import TaskList
-
+from .views import TaskList, TaskDetail,TaskCreate, TaskUpdate,TaskDelete,CustomLoginView,RegisterPage
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='tasks'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
+
     path('', TaskList.as_view(), name='tasks'),
+    path('task/<uuid:pk>/', TaskDetail.as_view(), name="task"),
+    path('task-create/', TaskCreate.as_view(), name="task-create"),
+    path('task-update/<uuid:pk>/', TaskUpdate.as_view(), name="task-update"),
+    path('task-delete/<uuid:pk>/', TaskDelete.as_view(), name="task-delete"),
+
 ]
